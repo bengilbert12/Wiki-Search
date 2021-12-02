2bc36173ecff7700147e5a14e5e38e4c43cf7204
Bengilbert12 <bgyonce@gmail.com>
2021-12-01 21:20:35 -0500

    initial commit

---
+++ main.py
@@ -0,0 +1,5 @@
+import wrapper
+
+title = str(input("Summarize:"))
+
+wrapper.search_wiki(title)
---
+++ wrapper.py
@@ -0,0 +1,25 @@
+import requests
+import urllib
+
+
+
+def search_wiki(title: str):
+    '''Summarizes requested Wikipedia page'''
+
+
+    title = urllib.parse.quote(title.lower())
+
+    r = requests.get(f'http://en.wikipedia.org/w/api.php?action=query&prop=extracts&exintro&explaintext&format=json&titles={title}')
+    
+    response = r.json()
+
+    pagenum = next(iter(response['query']['pages']))
+
+    try:
+        sum = response['query']['pages'][pagenum]['extract']
+        print(sum)
+    except KeyError:
+        print('Wiki page not found')
+    except:
+        print("Don't fuck with the program like that")
+
